ce391b9d76796af06b5095ec43727725e6920578
kajj8808 <a01039388808@gmail.com>
2024-12-19 04:27:11 +0900

    📝 stremlit chat stream 강의 메모

--- pages/01_DocumentGPT.py
+++ pages/01_DocumentGPT.py
@@ -1,34 +1,134 @@
 import time
 import streamlit as st
+from langchain.prompts import ChatPromptTemplate
+from langchain.chat_models import ChatOpenAI
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.storage import LocalFileStore
+from langchain.document_loaders import UnstructuredFileLoader
+from langchain.embeddings import OpenAIEmbeddings, CacheBackedEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.schema.runnable import RunnableLambda, RunnablePassthrough
+from langchain.chat_models import ChatOpenAI
+from langchain.callbacks.base import BaseCallbackHandler
 
 st.set_page_config(
     page_title="Document GPT",
     page_icon="📗"
 )
 
-st.title("DocumentGPT")
 
-if "messages" not in st.session_state:
-    st.session_state["messages"] = []
+class ChatCallbackHandler(BaseCallbackHandler):
+    message = ""
+
+    def on_llm_start(self, *args, **kwargs):
+        self.message_box = st.empty()
+
+    def on_llm_end(self, *args, **kwargs):
+        save_message(self.message, "ai")
+
+    def on_llm_new_token(self, token, *args, **kwargs):
+        self.message += token
+        self.message_box.markdown(self.message)
+
+
+llm = ChatOpenAI(
+    temperature=0.1,
+    streaming=True,
+    callbacks=[
+        ChatCallbackHandler(),
+    ]
+)
+
+
+@st.cache_data(show_spinner="Embedding file...")
+def embed_file(file):
+    file_content = file.read()
+    file_path = f"./.cache/files/{file.name}"
+
+    with open(file_path, "wb") as f:
+        f.write(file_content)
+
+    cache_dir = LocalFileStore(f"./.cache/embeddings/{file.name}")
+    splitter = CharacterTextSplitter.from_tiktoken_encoder(
+        separator="\n",
+        chunk_size=600,
+        chunk_overlap=100,
+    )
+    loader = UnstructuredFileLoader(file_path)
+    docs = loader.load_and_split(text_splitter=splitter)
+    embeddings = OpenAIEmbeddings()
+    cached_embeddings = CacheBackedEmbeddings.from_bytes_store(
+        embeddings, cache_dir,
+    )
+    vectorstore = FAISS.from_documents(docs, cached_embeddings)
+    retriever = vectorstore.as_retriever()
+
+    return retriever
+
+
+def save_message(message, role):
+    st.session_state["messages"].append({"message": message, "role": role})
 
 
 def send_message(message, role, save=True):
     with st.chat_message(role):
-        st.write(message)
+        st.markdown(message)
     if save:
-        st.session_state["messages"].append({"message": message, "role": role})
+        save_message(message, role)
 
 
-for message in st.session_state["messages"]:
-    send_message(message["message"], message["role"], save=False)
+def paint_history():
+    for message in st.session_state["messages"]:
+        send_message(message["message"], message["role"], save=False)
 
 
-message = st.chat_input("Send a message to the AI!")
+def format_docs(docs):
+    return "\n\n".join(document.page_content for document in docs)
 
-if message:
-    send_message(message, "human")
-    time.sleep(2)
-    send_message(f"You send: {message}", "ai")
 
-    with st.sidebar:
-        st.write(st.session_state)
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+            Answer the question using ONLY the following context. If you don't know the answer just say you don't know. DON'T make anything up.
+            
+            Context: {context}
+            """,
+        ),
+        ("human", "{question}"),
+    ]
+)
+
+st.title("DocumentGPT")
+
+st.markdown("""
+    Welcome!
+            
+    Use this chatbot to ask questions to an AI about your files!
+            
+    Upload yout files on the sidebar.
+""")
+with st.sidebar:
+    file = st.file_uploader(
+        label="Upload a .txt .pdf or .docx file",
+        type=["pdf", "txt", "doc"]
+    )
+
+if file:
+    retriever = embed_file(file)
+    send_message("I'm ready! Ask away! ", "ai", save=False)
+    paint_history()
+    message = st.chat_input("Ask Anything about your file...")
+    if message:
+        send_message(message, role="human")
+        chain = {
+            "context": retriever | RunnableLambda(format_docs),
+            "question": RunnablePassthrough(),
+        } | prompt | llm
+        with st.chat_message("ai"):
+            response = chain.invoke(message)
+
+
+else:
+    st.session_state["messages"] = []
