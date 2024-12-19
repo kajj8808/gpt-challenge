7d02c8e941a01faae856294571aa014a0c66a0ab
kajj8808 <a01039388808@gmail.com>
2024-12-20 02:37:22 +0900

    ✅ 이전 과제에서 구현한 chain 부분 작성

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Streamlit is 🔥 과제 제출용 레포지터리 입니다. (streamlit + GPT)
 
 ### 과제
 
-- [] 이전 과제에서 구현한 RAG 파이프라인을 Streamlit으로 마이그레이션합니다.
+- [x] 이전 과제에서 구현한 RAG 파이프라인을 Streamlit으로 마이그레이션합니다.
 - [x] 파일 업로드 및 채팅 기록을 구현합니다.
 - [x] 사용자가 자체 OpenAI API 키를 사용하도록 허용하고, st.sidebar 내부의 st.input에서 이를 로드합니다.
 - [] st.sidebar를 사용하여 스트림릿 앱의 코드와 함께 깃허브 리포지토리에 링크를 넣습니다.
--- app.py
+++ app.py
@@ -1,10 +1,25 @@
 import streamlit as st
 import time
 
+from langchain.storage import LocalFileStore
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.document_loaders import UnstructuredFileLoader
+from langchain.embeddings import OpenAIEmbeddings, CacheBackedEmbeddings
+from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.schema.runnable import RunnablePassthrough, RunnableLambda
+from langchain.memory import ConversationBufferMemory
+from langchain.callbacks.base import BaseCallbackHandler
+from langchain.chat_models import ChatOpenAI
+from langchain.vectorstores import FAISS
+
+
 MESSAGE_SESSION_KEY = "message_history"
 OPENAI_SESSION_KEY = "openai_session_key"
+MEMORY_SESSION_KEY = "langchain_buffer_memory"
 
 FILES_DIR = "./cache/files"
+EMBEDDING_DIR = "./cache/embeddings"
+
 
 st.set_page_config(
     page_icon="🔥",
@@ -14,12 +29,37 @@ st.set_page_config(
 
 st.title("Streamlit🔥")
 
-
-# 사용자가 자체 OpenAI API 키를 사용하도록 허용하고, st.sidebar 내부의 st.input에서 이를 로드합니다.
+if MEMORY_SESSION_KEY not in st.session_state:
+    st.session_state[MEMORY_SESSION_KEY] = ConversationBufferMemory(
+        return_messages=True,
+    )
 
 if OPENAI_SESSION_KEY not in st.session_state:
     st.session_state[OPENAI_SESSION_KEY] = ""
 
+
+class ChatCallbackHandler(BaseCallbackHandler):
+    message = ""
+
+    def on_llm_start(self, token, *args, **kwargs):
+        self.message_box = st.empty()
+
+    def on_llm_end(self, token, *args, **kwargs):
+        save_message(self.message, "ai")
+
+    def on_llm_new_token(self, token, *args, **kwargs):
+        self.message += token
+        self.message_box.markdown(self.message)
+
+
+chat = ChatOpenAI(
+    temperature=0.1,
+    streaming=True,
+    callbacks=[
+        ChatCallbackHandler(),
+    ]
+)
+memory = st.session_state[MEMORY_SESSION_KEY]
 
 with st.sidebar:
     st.markdown(""" 
@@ -30,7 +70,6 @@ with st.sidebar:
     OpenAI API 키를 입력 후 문서 파일을 업로드하면 대화가 시작됩니다!
     """)
 
-    # API 키 입력
     api_key_input = st.text_input(
         label="Open AI API를 입력해 주세요!",
         type="password",
@@ -40,7 +79,7 @@ with st.sidebar:
 
     if api_key_input and not st.session_state[OPENAI_SESSION_KEY]:
         st.session_state[OPENAI_SESSION_KEY] = api_key_input
-        st.experimental_rerun()  # 변경 사항을 반영하기 위해 새로고침
+        st.experimental_rerun()  # 변경이 안되는 문제가 생겨 새로고침!
 
     if st.session_state[OPENAI_SESSION_KEY]:
         file = st.file_uploader(
@@ -68,19 +107,79 @@ def paint_message_history():
         send_mseesage(message["message"], message["role"], save=False)
 
 
+@st.cache_data(show_spinner="문서 파일을 분석중입니다...")
+def embed_file(file):
+    file_content = file.read()
+    file_path = f"{FILES_DIR}/{file.name}"
+
+    with open(file_path, "wb") as f:
+        f.write(file_content)
+    # embedding 된 파일을 cache 할 dir 를 설정해줍니다.
+    cache_dir = LocalFileStore(f"{EMBEDDING_DIR}/{file.name}")
+    # text를 나눠줍니다.
+    splitter = CharacterTextSplitter.from_tiktoken_encoder(
+        separator="\n",
+        chunk_size=600,
+        chunk_overlap=100,
+    )
+
+    loader = UnstructuredFileLoader(file_path)
+    docs = loader.load_and_split(splitter)
+
+    embeddings = OpenAIEmbeddings()
+    # embeddings 파일로 cache
+    cached_embeddings = CacheBackedEmbeddings.from_bytes_store(
+        embeddings, cache_dir
+    )
+
+    vectorstore = FAISS.from_documents(docs, cached_embeddings)
+    retriever = vectorstore.as_retriever()
+    return retriever
+
+
+def format_docs(docs):
+    return "\n\n".join(document.page_content for document in docs)
+
+
+def load_memory(_):
+    return memory.load_memory_variables({})["history"]
+
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system",
+         "당신은 유용한 도우미입니다. 아래 제공된 문맥만을 사용하여 질문에 답하세요. 답을 모를 경우, 모른다고 말하세요. 답을 지어내지 마세요:\n\n{context}"),
+        MessagesPlaceholder(variable_name="history"),
+        ("human", "{question}")
+    ]
+)
+
+
+def invoke_chain(question):
+    chain = {
+        "context": retriever | RunnableLambda(format_docs),
+        "question": RunnablePassthrough(),
+        "history": load_memory
+    } | prompt | chat
+
+    result = chain.invoke(question)
+
+    memory.save_context(
+        {'input': message},
+        {'output': result.content},
+    )
+
+
 if st.session_state[OPENAI_SESSION_KEY]:
     if file:
-        file_content = file.read()
-        file_path = f"{FILES_DIR}/{file.name}"
-
-        with open(file_path, "wb") as f:
-            f.write(file_content)
+        retriever = embed_file(file)
 
         message = st.chat_input(f"{file.name}에 관해 물어보기")
         paint_message_history()
         if message:
             send_mseesage(message, "human")
-            time.sleep(2)
-            send_mseesage("hahaha", "ai")
+            with st.chat_message("ai"):
+                invoke_chain(message)
+
     else:
         st.session_state[MESSAGE_SESSION_KEY] = []
