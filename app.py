c9871f6b4cc50cd9291201fe18c83368c5d1def3
aaaa <71279997+kajj8808@users.noreply.github.com>
2024-12-23 03:49:13 +0900

    📝 Github 주소를 최종 수정 commit로 변경

--- app.py
+++ app.py
@@ -88,7 +88,7 @@ with st.sidebar:
         st.button("문제 생성", on_click=generate_quiz)
     st.markdown(
         """
-        [Github 링크](https://github.com/kajj8808/gpt-challenge/tree/c96446ed451b2c66ed850097624b958b8ee9a595)
+        [Github 링크](https://github.com/kajj8808/gpt-challenge/tree/4c6b343c05ef8ccbb2fc5071c4bcdf895e2c7590)
         """
     )
 
