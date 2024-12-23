1ef91508874e27e6d551c38696d47c9adb778b9e
kajj8808 <a01039388808@gmail.com>
2024-12-24 02:43:28 +0900

    📝 크로미움으로 site data 가져오는 방법 (AsyncChromiumLoader)

--- pages/04_SiteGPT.py
+++ pages/04_SiteGPT.py
@@ -1,3 +1,17 @@
 import streamlit as st
+from langchain.document_loaders import AsyncChromiumLoader
+from langchain.document_transformers import Html2TextTransformer
 
 st.title("QuizGPT")
+
+html2text_transformer = Html2TextTransformer()
+
+
+with st.sidebar:
+    url = st.text_input("url 을 입력해 주세요.!", placeholder="https://example.com")
+
+if url:
+    loader = AsyncChromiumLoader([url])
+    docs = loader.load()
+    transformed = html2text_transformer.transform_documents(docs)
+    st.write(docs)
